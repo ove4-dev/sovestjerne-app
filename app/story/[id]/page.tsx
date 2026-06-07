106fa1f1222a56dfd54179596ff714606ddb1538
ove4-dev <karlsen8@hotmail.com>
2026-06-07 14:08:06 +0200

    Update page.tsx

--- app/story/[id]/page.tsx
+++ app/story/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from '../../../../lib/supabaseAdmin';
+import { supabaseAdmin } from '../../../lib/supabaseAdmin';
 
 type StoryPageProps = {
   params: Promise<{ id: string }>;
