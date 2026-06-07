74ea3a9fd71c6185d0d661385d5ea22f8d2d5e64
ove4-dev <karlsen8@hotmail.com>
2026-06-07 03:47:13 +0200

    Update route.ts

--- app/api/admin/route.ts
+++ app/api/admin/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { supabaseAdmin } from '@/lib/supabaseAdmin';
+import { supabaseAdmin } from '../../../lib/supabaseAdmin';
 
 export async function GET(request: Request) {
   const adminPassword = process.env.ADMIN_PASSWORD;
