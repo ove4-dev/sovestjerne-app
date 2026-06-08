af522c01b4df5f1db004f978fc1bf4e7ee766d4f
ove4-dev <karlsen8@hotmail.com>
2026-06-08 04:13:52 +0200

    Update page.tsx

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FormEvent, useEffect, useState } from 'react';
+import { FormEvent, Suspense, useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 
 const interests = ['Dyr', 'Magi', 'Verdensrommet', 'Dinosaurer', 'Hester', 'Fotball', 'Eventyr', 'Pirater', 'Prinsesser', 'Havfruer', 'Biler', 'Tog'];
